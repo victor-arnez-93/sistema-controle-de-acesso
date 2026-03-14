57c5097849aeffbbcdc92c38aeb8e41b64ae3c7d
Victor Arnez <136041084+victor-arnez-93@users.noreply.github.com>
2026-03-14 00:15:18 -0300

    update app.py

--- crv-acesso/app.py
+++ crv-acesso/app.py
@@ -1,14 +1,16 @@
+import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/')
 def index():
     return send_from_directory('.', 'dashboard.html')
 
-@app.route('/<path:filename>')
-def serve_file(filename):
-    return send_from_directory('.', filename)
+@app.route('/<path:path>')
+def serve(path):
+    return send_from_directory('.', path)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
