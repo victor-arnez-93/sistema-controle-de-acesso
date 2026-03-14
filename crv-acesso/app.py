914c55ba9eb5ab4892f218e13855e5b9407c4979
Victor Arnez <136041084+victor-arnez-93@users.noreply.github.com>
2026-03-14 16:55:43 -0300

    update

--- crv-acesso/app.py
+++ crv-acesso/app.py
@@ -15,5 +15,5 @@ def serve(path):
         abort(404)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
